6dd3b22488fe973d7b8fbdfc0981312d7c27d11c
Ruven <ruven@uni-bremen.de>
2023-08-30 22:51:20 +0200

    fixed Model not displaying

--- src/main/java/net/Pandarix/betterarcheology/block/custom/ChickenFossilBlock.java
+++ src/main/java/net/Pandarix/betterarcheology/block/custom/ChickenFossilBlock.java
@@ -12,6 +12,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -49,6 +50,11 @@ public class ChickenFossilBlock extends FossilBaseWithEntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new ChickenFossilBlockEntity(pos, state);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
     }
 
     @OnlyIn(Dist.CLIENT)
