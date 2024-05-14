217c9f27c6d24294d0ed759285f2486899129df4
Ruven <ruven@uni-bremen.de>
2024-05-14 14:02:48 +0200

    fix: Replaced Alex's Mobs workaround with compatibility check - resolving #73

--- src/main/java/net/Pandarix/betterarcheology/enchantment/SoaringWindsEnchantment.java
+++ src/main/java/net/Pandarix/betterarcheology/enchantment/SoaringWindsEnchantment.java
@@ -1,22 +1,38 @@
 package net.Pandarix.betterarcheology.enchantment;
 
+import com.github.alexthe666.alexsmobs.item.ItemTarantulaHawkElytra;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraftforge.fml.ModList;
 
-public class SoaringWindsEnchantment extends ArtifactEnchantment {
-    protected SoaringWindsEnchantment(Rarity weight, EnchantmentCategory target, EquipmentSlot... slotTypes) {
+public class SoaringWindsEnchantment extends ArtifactEnchantment
+{
+    protected SoaringWindsEnchantment(Rarity weight, EnchantmentCategory target, EquipmentSlot... slotTypes)
+    {
         super(weight, target, slotTypes);
     }
 
     @Override
-    public int getMaxLevel() {
+    public int getMaxLevel()
+    {
         return 1;
     }
 
-    // We're able to pass null here because the entity is not used in the check as of 1.20
     @Override
-    public boolean canEnchant(ItemStack pStack) {
-        return pStack.canElytraFly(null);
+    public boolean canEnchant(ItemStack pStack)
+    {
+        if (pStack.getItem() instanceof ElytraItem)
+        {
+            return true;
+        }
+        // IF CURIOSAPI is installed
+        if (ModList.get().isLoaded("alexsmobs"))
+        {
+            return pStack.getItem() instanceof ItemTarantulaHawkElytra;
+        }
+
+        return false;
     }
 }
